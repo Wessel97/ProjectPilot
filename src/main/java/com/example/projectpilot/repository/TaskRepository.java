8df5450d0acd8b134997c716c0520c79d298ee1c
Frederik Wessel <frederikglimsager@hotmail.com>
2023-05-04 12:36:28 +0200

    update taskrepository.java

--- src/main/java/com/example/projectpilot/repository/TaskRepository.java
+++ src/main/java/com/example/projectpilot/repository/TaskRepository.java
@@ -24,13 +24,65 @@ public class TaskRepository {
         // SQL Query + Connection Try Catch
     }
 
-    public Task getTaskById(int taskId) {
+    public Task findTaskById(int taskId) {
         Task task = new Task();
         // SQL Query + Connection Try Catch
         return task;
     }
 
+    // Sort Metoder
+
+    public void sortByHours() {
+        /*
+        Skal sortere tasks efter timer
+         */
+    }
+
+    public void sortByDepartment() {
+        /*
+        Skal sortere tasks efter afdeling
+         */
+    }
+
+    public void sortByStatus() {
+        /*
+        Skal sortere tasks efter status
+         */
+    }
+
+    public void sortByFlag() {
+        /*
+        Skal sortere tasks efter flag
+         */
+    }
 
 
+    public int timeOverview() {
+        /*
+        Skal vise resultatet af totale timer der er
+        brugt og er til overs i tasks
+         */
+
+        // int totalHours = ???
+
+        // return totalHours;
+        return  0;
+    }
+
+    public int priceOverview() {
+        /*
+        Skal vise resultatet af totale timer der er
+        brugt og er til overs i tasks, ganget med
+        gennesnitlig timeløn (payRate)
+         */
+
+        // int payRate = 300;
+        // int totalHours = ???
+
+        // int price = totalHours * payRate;
+
+        // return price;
+        return 0;
+    }
 
 }
