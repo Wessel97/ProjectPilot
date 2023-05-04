62d52940083493475c0c23a09ca55ca45f91e912
Christian <107435301+PedroLorenzen@users.noreply.github.com>
2023-05-04 13:27:29 +0200

    Commit userRepository & User

--- src/main/java/com/example/projectpilot/repository/UserRepository.java
+++ src/main/java/com/example/projectpilot/repository/UserRepository.java
@@ -18,13 +18,13 @@ public class UserRepository
 
     //Method 1 extract user from SQL. This method will return a user object from the database.
     private User extractUser(ResultSet resultSet) throws SQLException {
-        int id = resultSet.getInt(1);
-        String fname = resultSet.getString(2);
-        String lname = resultSet.getString(3);
+        int user_id = resultSet.getInt(1);
+        String first_name = resultSet.getString(2);
+        String last_name = resultSet.getString(3);
         String email = resultSet.getString(4);
-        String pw = resultSet.getString(5);
-        User user = new User(fname, lname, email, pw);
-        user.setId(id);
+        String password = resultSet.getString(5);
+        User user = new User(first_name, last_name, email, password);
+        user.setID(user_id);
         return user;
     }
 
@@ -49,41 +49,36 @@ public class UserRepository
     }
 
     //Method 3 check if user exists. This method will return true if the user exists in the database.
-    public boolean checkIfUserExists(String eMail)
-    {
+    public boolean checkIfUserExists(String checkEmail) {
         final String FIND_QUERY = "SELECT * FROM ProjectPilotDB.user WHERE email = ?";
-        try
-        {
+        try {
             //db connection
             Connection connection = DriverManager.getConnection(DB_URL, UID, PWD);
 
             //prepared statement
             PreparedStatement preparedStatement = connection.prepareStatement(FIND_QUERY);
-            preparedStatement.setString(1, eMail);
+            preparedStatement.setString(1, checkEmail);
             //execute statement
             ResultSet resultSet = preparedStatement.executeQuery();
 
-            while(resultSet.next())
-            {
-                String email = resultSet.getString(4);
-                if(email != null)
-                {
+            while(resultSet.next()) {
+                String DB_email = resultSet.getString(4);
+                if(DB_email != null && DB_email.equals(checkEmail)) {
                     return true;
                 }
             }
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             System.out.println("Could not query database");
             e.printStackTrace();
         }
         return false;
     }
 
+
     //Method 4 add user. This method will return true if the user was successfully added to the database.
     public boolean addUser(User user)
     {
-        final String INSERT_QUERY = "INSERT INTO ProjectPilotDB.user (fname, lname, email, pw) VALUES (?, ?, ?, ?)";
+        final String INSERT_QUERY = "INSERT INTO ProjectPilotDB.user (first_name, last_name, email, password) VALUES (?, ?, ?, ?)";
         try
         {
             //db connection
@@ -91,10 +86,10 @@ public class UserRepository
 
             //prepared statement
             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_QUERY);
-            preparedStatement.setString(1, user.getfname());
-            preparedStatement.setString(2, user.getlname());
+            preparedStatement.setString(1, user.getFirstName());
+            preparedStatement.setString(2, user.getLastName());
             preparedStatement.setString(3, user.getEmail());
-            preparedStatement.setString(4, user.getPw());
+            preparedStatement.setString(4, user.getPassword());
 
             //execute statement
             int rowsAffected = preparedStatement.executeUpdate();
@@ -112,15 +107,15 @@ public class UserRepository
     }
 
     //Method 5 get user by ID. This method will return a user object if the user exists in the database.
-    public User getUserByID(String id) {
-        final String FIND_QUERY = "SELECT * FROM ProjectPilotDB.user WHERE id = ?";
+    public User getUserByID(String user_id) {
+        final String FIND_QUERY = "SELECT * FROM ProjectPilotDB.user WHERE user_id = ?";
         try {
             //db connection
             Connection connection = DriverManager.getConnection(DB_URL, UID, PWD);
 
             //prepared statement
             PreparedStatement preparedStatement = connection.prepareStatement(FIND_QUERY);
-            preparedStatement.setString(1, id);
+            preparedStatement.setString(1, user_id);
             //execute statement
             ResultSet resultSet = preparedStatement.executeQuery();
 
@@ -137,7 +132,7 @@ public class UserRepository
     // Method 6 update user. This method will return true if the user was successfully updated in the database.
     public void updateUser(User user)
     {
-        final String UPDATE_QUERY = "UPDATE ProjectPilotDB.user SET fname = ?, lname = ?, email = ?, pw = ? WHERE id = ?";
+        final String UPDATE_QUERY = "UPDATE ProjectPilotDB.user SET first_name = ?, last_name = ?, email = ?, password = ? WHERE user_id = ?";
         try
         {
             //db connection
@@ -145,11 +140,11 @@ public class UserRepository
 
             //prepared statement
             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_QUERY);
-            preparedStatement.setString(1, user.getfname());
-            preparedStatement.setString(2, user.getlname());
+            preparedStatement.setString(1, user.getFirstName());
+            preparedStatement.setString(2, user.getLastName());
             preparedStatement.setString(3, user.getEmail());
-            preparedStatement.setString(4, user.getPw());
-            preparedStatement.setInt(5, user.getId());
+            preparedStatement.setString(4, user.getPassword());
+            preparedStatement.setInt(5, user.getID());
 
             //execute statement
             preparedStatement.executeUpdate();
@@ -162,39 +157,54 @@ public class UserRepository
     }
 
     // Method 7 verify user. This method will return true if the user exists in the database.
-    public boolean verifyUser(String email, String pw)
-    {
-        final String FIND_QUERY = "SELECT * FROM ProjectPilotDB.user WHERE email = ? AND pw = ?";
-        try
-        {
+    public boolean verifyUser(String email, String password) {
+        final String FIND_QUERY = "SELECT * FROM ProjectPilotDB.user WHERE email = ? AND password = ?";
+        boolean userExists = false;
+        try {
+            // Establish a database connection
+            Connection connection = DriverManager.getConnection(DB_URL, UID, PWD);
+
+            // Prepare the SQL query
+            PreparedStatement preparedStatement = connection.prepareStatement(FIND_QUERY);
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, password);
+
+            // Execute the query and retrieve the result
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            // Check if a user with the given email and password exists
+            if (resultSet.next()) {
+                userExists = true;
+            }
+        } catch (SQLException e) {
+            System.out.println("Could not query database");
+            e.printStackTrace();
+        }
+
+        return userExists;
+    }
+
+    //Method 7 delete user by ID. This method will return true if the user was successfully deleted from the database.
+    //Method 7 delete user by ID. This method will return true if the user was successfully deleted from the database.
+    public boolean deleteUserByID(User user) {
+        final String DELETE_QUERY = "DELETE FROM ProjectPilotDB.user WHERE user_id = ?";
+        try {
             //db connection
             Connection connection = DriverManager.getConnection(DB_URL, UID, PWD);
 
             //prepared statement
-            PreparedStatement preparedStatement = connection.prepareStatement(FIND_QUERY);
-            preparedStatement.setString(1, email);
-            preparedStatement.setString(2, pw);
-            //execute statement
-            ResultSet resultSet = preparedStatement.executeQuery();
+            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_QUERY);
+            preparedStatement.setInt(1, user.getID());
 
-            while(resultSet.next()){
-                email = resultSet.getString(4);
-                pw = resultSet.getString(5);
-                if(email != null && password != null){
-                    return true;
-                }
+            //execute statement
+            int foundUser = preparedStatement.executeUpdate();
+            if(foundUser == 1) {
+                return true;
             }
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             System.out.println("Could not query database");
             e.printStackTrace();
         }
         return false;
-    }
-
-    public deleteUserByID()
-    {
-
     }
 }
