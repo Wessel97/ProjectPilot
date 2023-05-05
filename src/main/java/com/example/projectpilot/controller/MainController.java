589a24af978c4c116e572a8267ab0024f4a8003a
Frederik Wessel <frederikglimsager@hotmail.com>
2023-05-05 12:04:27 +0200

    list main controller og html

--- src/main/java/com/example/projectpilot/controller/MainController.java
+++ src/main/java/com/example/projectpilot/controller/MainController.java
@@ -1,4 +1,32 @@
 package com.example.projectpilot.controller;
 
+import com.example.projectpilot.repository.TaskRepository;
+import com.example.projectpilot.repository.UserRepository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class MainController {
+
+    private UserRepository userRepository;
+    private TaskRepository taskRepository;
+
+    public MainController(UserRepository userRepository, TaskRepository taskRepository) {
+        this.userRepository = userRepository;
+        this.taskRepository = taskRepository;
+    }
+
+
+    @GetMapping("/")
+    public String showUserList(Model model) {
+        model.addAttribute("user", userRepository.getAllUsers());
+        return "userlist";
+    }
+
+
+
+
+
+
+
+
 }
