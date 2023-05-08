c10238dcbb340fe6e1467d78ed83a42f8034c3ab
Frederik Wessel <frederikglimsager@hotmail.com>
2023-05-08 10:31:17 +0200

    homecontroller

--- src/main/java/com/example/projectpilot/controller/MainController.java
+++ src/main/java/com/example/projectpilot/controller/MainController.java
@@ -2,9 +2,12 @@ package com.example.projectpilot.controller;
 
 import com.example.projectpilot.repository.TaskRepository;
 import com.example.projectpilot.repository.UserRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class MainController {
 
     private UserRepository userRepository;
@@ -15,12 +18,44 @@ public class MainController {
         this.taskRepository = taskRepository;
     }
 
-
-    @GetMapping("/")
-    public String showUserList(Model model) {
-        model.addAttribute("user", userRepository.getAllUsers());
-        return "userlist";
+    public MainController(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
+
+    public MainController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public MainController () {
+    }
+
+    // Viser startsiden
+    @GetMapping("/")
+    public String showForside(Model model) {
+        return "start";
+    }
+
+    // Viser alle Tasks
+    @GetMapping ("/allTasks")
+    public String showAllTasks(Model model) {
+        model.addAttribute("tasks", taskRepository.getAllTasks());
+        return "allTasks";
+    }
+
+    // Viser alle tasks for den enkelte users (MANGLER MODE)
+    @GetMapping ("/userTasks")
+    public String showUserTasks(Model model) {
+        model.addAttribute("tasks", taskRepository.getAllTasksByUserID();
+        return "userTasks";
+    }
+
+
+
+
+
+
+
+
 
 
 
