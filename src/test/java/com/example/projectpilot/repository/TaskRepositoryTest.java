93680c3117637ecdd10b5ad8c66e6657612ee5da
Frederik Wessel <frederikglimsager@hotmail.com>
2023-05-28 23:04:09 +0200

    testAddTask

--- src/test/java/com/example/projectpilot/repository/TaskRepositoryTest.java
+++ src/test/java/com/example/projectpilot/repository/TaskRepositoryTest.java
@@ -2,66 +2,60 @@ package com.example.projectpilot.repository;
 
 import com.example.projectpilot.model.Task;
 import com.example.projectpilot.service.DatabaseService;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.MockitoAnnotations;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import static org.mockito.ArgumentMatchers.*;
+import java.sql.Date;
+
 import static org.mockito.Mockito.*;
 
-
-@ExtendWith(MockitoExtension.class)
 public class TaskRepositoryTest {
+
     @Mock
     private DatabaseService databaseService;
+
     @Mock
     private Connection connection;
+
     @Mock
     private PreparedStatement preparedStatement;
 
     private TaskRepository taskRepository;
 
-
     @BeforeEach
-    public void setup() throws SQLException {
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
         taskRepository = new TaskRepository(databaseService);
-        when(databaseService.getConnection()).thenReturn(connection);
-        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
     }
+
+    //  
 
     @Test
     public void testAddTask() throws SQLException {
-        // Prepare test data
         Task task = new Task();
-        // Set task properties
         task.setDepartment_id(1);
-        task.setTitle("Task Title");
-        task.setDescription("Task Description");
-        task.setNote("Task Note");
+        task.setTitle("Test Title");
+        task.setDescription("Test Description");
+        task.setNote("Test Note");
         task.setHours(5);
-        task.setStart_Date(java.sql.Date.valueOf("2021-01-01"));
-        task.setEnd_Date(java.sql.Date.valueOf("2021-01-02"));
-        task.setStatus("Task Status");
-        task.setDepartment("Task Department");
-        // Call the method
+        task.setStart_Date(Date.valueOf("2021-05-05"));
+        task.setEnd_Date(Date.valueOf("2021-05-05"));
+        task.setStatus("Pending");
+        task.setDepartment("Test Department");
+
+        when(databaseService.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(any(String.class))).thenReturn(preparedStatement);
+
         taskRepository.addTask(task);
 
-        // Verify the interactions with mocks
-        verify(databaseService).getConnection();
-        verify(connection).prepareStatement(eq("INSERT INTO task(department_id, title, description, note, hours, start_date, end_date, status, department) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"));
-        verify(preparedStatement).setInt(eq(1), eq(task.getDepartment_id()));
-        verify(preparedStatement).setString(eq(2), eq(task.getTitle()));
-        verify(preparedStatement).setString(eq(3), eq(task.getDescription()));
-        verify(preparedStatement).setString(eq(4), eq(task.getNote()));
-        verify(preparedStatement).setInt(eq(5), eq(task.getHours()));
-        verify(preparedStatement).setDate(eq(6), eq(task.getStart_Date()));
-        verify(preparedStatement).setDate(eq(7), eq(task.getEnd_Date()));
-        verify(preparedStatement).setString(eq(8), eq(task.getStatus()));
-        verify(preparedStatement).setString(eq(9), eq(task.getDepartment()));
-        verify(preparedStatement).executeUpdate();
+        verify(databaseService, times(1)).getConnection();
+        verify(connection, times(1)).prepareStatement(any(String.class));
+        verify(preparedStatement, times(1)).executeUpdate();
     }
+
 }
